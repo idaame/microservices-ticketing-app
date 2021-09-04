2ba95026becae6fc3b4df99037212c46a443ac33
Ida Merete Enholm <ida.enholm@me.com>
2021-09-04 18:19:24 +0200

    don't cancel completed orders

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -3,22 +3,27 @@ import {
   Subjects,
   ExpirationCompleteEvent,
   OrderStatus,
-} from "@imetickets/common";
-import { Message } from "node-nats-streaming";
-import { queueGroupName } from "./queue-group-name";
-import { Order } from "../../models/order";
-import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
+} from '@imetickets/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+import { Order } from '../../models/order';
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher';
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
   queueGroupName = queueGroupName;
   subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
 
-  async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
-    const order = await Order.findById(data.orderId).populate("ticket");
+  async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+    const order = await Order.findById(data.orderId).populate('ticket');
 
     if (!order) {
-      throw new Error("Order not found");
+      throw new Error('Order not found');
     }
+
+    if (order.status === OrderStatus.Complete) {
+      return msg.ack();
+    }
+
     order.set({
       status: OrderStatus.Cancelled,
     });
