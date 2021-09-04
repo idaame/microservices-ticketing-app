cfd15c9cd8f1b2535d5c25a929e5ab3d4ff4b4aa
Ida Merete Enholm <ida.enholm@me.com>
2021-09-04 14:57:33 +0200

    Create order created listener

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -1,61 +1,57 @@
-import { MongoMemoryServer } from "mongodb-memory-server";
-import mongoose from "mongoose";
-import { app } from "../app";
-import request from "supertest";
-import jwt from "jsonwebtoken";
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+import request from 'supertest';
+import jwt from 'jsonwebtoken';
 
 declare global {
-  namespace NodeJS {
-    interface Global {
-      signin(): string[];
-    }
-  }
+    var signin: (id?: string) => string[];
 }
 
 let mongo: any;
 
-jest.mock("../nats-wrapper");
+jest.mock('../nats-wrapper');
 
 beforeAll(async () => {
-  process.env.JWT_KEY = "asdfasdf";
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    process.env.JWT_KEY = 'asdfasdf';
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-  mongo = new MongoMemoryServer();
-  const mongoUri = await mongo.getUri();
+    mongo = new MongoMemoryServer();
+    const mongoUri = await mongo.getUri();
 
-  await mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+    await mongoose.connect(mongoUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
 });
 
 beforeEach(async () => {
-  jest.clearAllMocks();
-  const collections = await mongoose.connection.db.collections();
+    jest.clearAllMocks();
+    const collections = await mongoose.connection.db.collections();
 
-  for (let collection of collections) {
-    await collection.deleteMany({});
-  }
+    for (let collection of collections) {
+        await collection.deleteMany({});
+    }
 });
 
 afterAll(async () => {
-  await mongo.stop();
-  await mongoose.connection.close;
+    await mongo.stop();
+    await mongoose.connection.close;
 });
 
 global.signin = () => {
-  const payload = {
-    id: new mongoose.Types.ObjectId().toHexString(),
-    email: "test@test.com",
-  };
+    const payload = {
+        id: new mongoose.Types.ObjectId().toHexString(),
+        email: 'test@test.com',
+    };
 
-  const token = jwt.sign(payload, process.env.JWT_KEY!);
+    const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-  const session = { jwt: token };
+    const session = { jwt: token };
 
-  const sessionJSON = JSON.stringify(session);
+    const sessionJSON = JSON.stringify(session);
 
-  const base64 = Buffer.from(sessionJSON).toString("base64");
+    const base64 = Buffer.from(sessionJSON).toString('base64');
 
-  return [`express:sess=${base64}`];
+    return [`express:sess=${base64}`];
 };
