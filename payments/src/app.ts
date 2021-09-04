71e121255e9fe39028380858d44886a1818f4a98
Ida Merete Enholm <ida.enholm@me.com>
2021-09-04 16:00:14 +0200

    add create charge route

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -1,23 +1,26 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
-import cookieSession from "cookie-session";
-import { errorHandler, NotFoundError, currentUser } from "@imetickets/common";
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@imetickets/common';
+import { createChargeRouter } from './routes/new';
 
 const app = express();
-app.set("trust proxy", true);
+app.set('trust proxy', true);
 app.use(json());
 app.use(
-  cookieSession({
-    signed: false,
-    secure: process.env.NODE_ENV !== "test",
-  })
+    cookieSession({
+        signed: false,
+        secure: process.env.NODE_ENV !== 'test',
+    })
 );
 
 app.use(currentUser);
 
-app.get("*", async (req, res) => {
-  throw new NotFoundError();
+app.use(createChargeRouter);
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
 });
 
 app.use(errorHandler);
--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -5,7 +5,7 @@ import request from 'supertest';
 import jwt from 'jsonwebtoken';
 
 declare global {
-    var signin: (id?: string) => string[];
+  var signin: (id?: string) => string[];
 }
 
 let mongo: any;
@@ -13,45 +13,45 @@ let mongo: any;
 jest.mock('../nats-wrapper');
 
 beforeAll(async () => {
-    process.env.JWT_KEY = 'asdfasdf';
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+  process.env.JWT_KEY = 'asdfasdf';
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-    mongo = new MongoMemoryServer();
-    const mongoUri = await mongo.getUri();
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
 
-    await mongoose.connect(mongoUri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 });
 
 beforeEach(async () => {
-    jest.clearAllMocks();
-    const collections = await mongoose.connection.db.collections();
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
 
-    for (let collection of collections) {
-        await collection.deleteMany({});
-    }
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
 });
 
 afterAll(async () => {
-    await mongo.stop();
-    await mongoose.connection.close;
+  await mongo.stop();
+  await mongoose.connection.close;
 });
 
-global.signin = () => {
-    const payload = {
-        id: new mongoose.Types.ObjectId().toHexString(),
-        email: 'test@test.com',
-    };
+global.signin = (id?: string) => {
+  const payload = {
+    id: id || new mongoose.Types.ObjectId().toHexString(),
+    email: 'test@test.com',
+  };
 
-    const token = jwt.sign(payload, process.env.JWT_KEY!);
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-    const session = { jwt: token };
+  const session = { jwt: token };
 
-    const sessionJSON = JSON.stringify(session);
+  const sessionJSON = JSON.stringify(session);
 
-    const base64 = Buffer.from(sessionJSON).toString('base64');
+  const base64 = Buffer.from(sessionJSON).toString('base64');
 
-    return [`express:sess=${base64}`];
+  return [`express:sess=${base64}`];
 };
